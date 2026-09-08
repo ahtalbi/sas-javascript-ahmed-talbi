92bbc1347a5e12b1496e3682be9833937b6bf31f
ahtalbi <obetox1@gmail.com>
2026-09-08 12:40:37 +0200

    guides

---
+++ day01/day01/guides/guide-01.js
@@ -0,0 +1,23 @@
+/**
+ * JOUR 01 — GUIDE 01
+ * Le Permutateur (Variables)
+ *
+ * OBJECTIF
+ * Vous avez deux variables a = 5 et b = 10. Vous devez échanger leurs valeurs pour que a vaille 10 et b vaille 5.
+ *
+ * Aide : consulte ../02-guides.md seulement si tu bloques.
+ */
+'use strict';
+
+let a = 5;
+let b = 10;
+
+console.log("before : ", a, b);
+// il ya deux solution
+let temp = a;
+a = b;
+b = temp;
+
+// le dexieme
+// [a, b] = [b, a];
+console.log("after  : ", a, b);
