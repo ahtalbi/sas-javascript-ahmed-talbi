ef6b5dfe4c6b48fd62e5be8a69327fff9d1fc189
ahtalbi <obetox1@gmail.com>
2026-09-08 16:04:00 +0200

    exercise 16

--- day01/exercices/exercice-16.js
+++ day01/exercices/exercice-16.js
@@ -5,7 +5,10 @@
  * ─────────────────────────────────────────────────────────────
  *
  * 🎯 MISSION
- * Vous attendez un numéro de téléphone sous forme de texte (ex: "0612345678"). Écrivez une logique qui vérifie que la valeur fournie est bien de type string ET que sa longueur est exactement de 10 caractères. Sinon, affichez "Numéro invalide".
+ * Vous attendez un numéro de téléphone sous forme de texte (ex: "0612345678"). 
+ * Écrivez une logique qui vérifie que la valeur fournie est bien de type string
+ * ET que sa longueur est exactement de 10 caractères. Sinon,
+ * affichez "Numéro invalide".
  *
  * 📖 Consigne détaillée : ../03-exercices.md#exercice-16
  * ▶️ Commande : node day01/exercices/exercice-16.js
@@ -14,4 +17,7 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+let txt = "0612345678";
+if (txt.length != 10) {
+    console.log("Numéro invalide");
+}
