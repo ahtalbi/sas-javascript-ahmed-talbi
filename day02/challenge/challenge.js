1745d96dcdd49c06e1d439056067141558c2d244
Ahmed <128240067+ahtalbi@users.noreply.github.com>
2026-09-09 15:45:33 +0200

    Update challenge.js

--- day02/challenge/challenge.js
+++ day02/challenge/challenge.js
@@ -27,10 +27,10 @@ for (let i = 0; i <= 9; i++) {
     for (let j = 0; j <= 9; j++) {
         for (let k = 0; k <= 9; k++) {
             let number = i * 100 + j * 10 + k;
+            counter++;
             if (number === codeSecret) {
                 console.log(`Code trouvé : 739 après ${counter} essais`);
             }
-            counter++;
         }
     }
 }
