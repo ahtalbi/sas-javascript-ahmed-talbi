da01c02d62ed5996a8c3602e4cb40787b65f078b
ahtalbi <obetox1@gmail.com>
2026-09-15 10:03:21 +0100

    exercise 12

--- day06/exercices/exercice-11.js
+++ day06/exercices/exercice-11.js
@@ -14,4 +14,9 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+let original = { a: 1, b: 2 };
+let clone = {...original};
+clone["c"] = 3;
+console.log(original);
+console.log(clone);
+
--- day06/exercices/exercice-12.js
+++ day06/exercices/exercice-12.js
@@ -14,4 +14,14 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+let compteBancaire = {
+    solde: 2000,
+
+    deposer(montant) {
+        this.solde += montant;
+    }
+};
+
+console.log(compteBancaire.solde);
+compteBancaire.deposer(10);
+console.log(compteBancaire.solde);
